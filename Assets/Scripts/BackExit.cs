100e2eddeb200a2a437a6de41f30c66c17558ddc
kabir071 <58852131+kabir071@users.noreply.github.com>
2020-12-27 23:12:24 +0530

    Initial Commit

---
+++ Assets/Scripts/BackExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackExit : MonoBehaviour
+{
+    public void back()
+    {
+        SceneManager.LoadScene("MenuImage");
+    }
+
+   
+
+    public void exitgame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   public void greetingcard()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void snowman()
+    {
+        SceneManager.LoadScene("Rotate");
+    }
+
+    public void exitgame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
